efe50d211c359d52fbdefa57ee8e86445315f898
sean <sean.s.elias@outlook.com>
2022-10-23 22:46:06 +1100

    first commit

---
+++ Domain/User.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [MaxLength(50)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(128)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string Passsword { get; set; }
+
+        [Required(ErrorMessage = "Firstname is required")]
+        [StringLength(100)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        [StringLength(100)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(256)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string Mobile { get; set; }
+    }
+}
---
+++ Persistence/Migrations/20221023110011_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(MAX)", maxLength: 50, nullable: false),
+                    Passsword = table.Column<string>(type: "nvarchar(MAX)", maxLength: 128, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(MAX)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(MAX)", maxLength: 100, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(MAX)", maxLength: 256, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(MAX)", maxLength: 50, nullable: false),
+                    Mobile = table.Column<string>(type: "nvarchar(MAX)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
